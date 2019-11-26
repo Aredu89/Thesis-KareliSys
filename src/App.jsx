4a079874c9a772fcf5525e88feb9b7c0b81cc8da
Aredu89 <aredu_r89@hotmail.com>
2019-11-25 22:21:08 -0300

    Se agrega React-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+
 import IssueList from './IssueList.jsx'
+import Link from './Link.jsx'
 
 const contentNode = document.getElementById('contents')
+const noMatch = () => <p>Page Not Found</p>
 
-ReactDOM.render(<IssueList />, contentNode) 
-// Render the component inside the content Node
+const RoutedApp = () => (
+  <Router history={hashHistory} >
+    <Route path="/" component={IssueList} />
+    <Route path="/link" component={Link} />
+    <Route path="*" component={noMatch} />
+  </Router>
+)
+
+ReactDOM.render(<RoutedApp />, contentNode) // Render the component inside the content Node
+//Para solucionar error, volver React y react-dom a version anterior
